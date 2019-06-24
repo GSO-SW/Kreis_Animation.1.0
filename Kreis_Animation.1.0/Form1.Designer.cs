5fa87c459bdac0ad51215240f7a27e430e8fc578
Mert Alkan <Mert Alkan@DESKTOP-PJH7KCS>
2019-06-24 18:00:44 +0200

    erster commit

---
+++ Kreis_Animation.1.0/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Kreis_Animation._1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.speedLabel = new System.Windows.Forms.Label();
+            this.speedTrackBar = new System.Windows.Forms.TrackBar();
+            this.radiusLabel = new System.Windows.Forms.Label();
+            this.radiusTrackBar = new System.Windows.Forms.TrackBar();
+            this.sizeLabel = new System.Windows.Forms.Label();
+            this.sizeTrackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.speedTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radiusTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // speedLabel
+            // 
+            this.speedLabel.AutoSize = true;
+            this.speedLabel.Location = new System.Drawing.Point(28, 24);
+            this.speedLabel.Name = "speedLabel";
+            this.speedLabel.Size = new System.Drawing.Size(83, 13);
+            this.speedLabel.TabIndex = 0;
+            this.speedLabel.Text = "geschwindigekit";
+            // 
+            // speedTrackBar
+            // 
+            this.speedTrackBar.Location = new System.Drawing.Point(117, 24);
+            this.speedTrackBar.Name = "speedTrackBar";
+            this.speedTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.speedTrackBar.TabIndex = 1;
+            // 
+            // radiusLabel
+            // 
+            this.radiusLabel.AutoSize = true;
+            this.radiusLabel.Location = new System.Drawing.Point(28, 98);
+            this.radiusLabel.Name = "radiusLabel";
+            this.radiusLabel.Size = new System.Drawing.Size(40, 13);
+            this.radiusLabel.TabIndex = 2;
+            this.radiusLabel.Text = "Radius";
+            // 
+            // radiusTrackBar
+            // 
+            this.radiusTrackBar.Location = new System.Drawing.Point(117, 84);
+            this.radiusTrackBar.Name = "radiusTrackBar";
+            this.radiusTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.radiusTrackBar.TabIndex = 3;
+            // 
+            // sizeLabel
+            // 
+            this.sizeLabel.AutoSize = true;
+            this.sizeLabel.Location = new System.Drawing.Point(31, 161);
+            this.sizeLabel.Name = "sizeLabel";
+            this.sizeLabel.Size = new System.Drawing.Size(36, 13);
+            this.sizeLabel.TabIndex = 4;
+            this.sizeLabel.Text = "Größe";
+            // 
+            // sizeTrackBar
+            // 
+            this.sizeTrackBar.Location = new System.Drawing.Point(117, 146);
+            this.sizeTrackBar.Name = "sizeTrackBar";
+            this.sizeTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.sizeTrackBar.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 450);
+            this.Controls.Add(this.sizeTrackBar);
+            this.Controls.Add(this.sizeLabel);
+            this.Controls.Add(this.radiusTrackBar);
+            this.Controls.Add(this.radiusLabel);
+            this.Controls.Add(this.speedTrackBar);
+            this.Controls.Add(this.speedLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.speedTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radiusTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label speedLabel;
+        private System.Windows.Forms.TrackBar speedTrackBar;
+        private System.Windows.Forms.Label radiusLabel;
+        private System.Windows.Forms.TrackBar radiusTrackBar;
+        private System.Windows.Forms.Label sizeLabel;
+        private System.Windows.Forms.TrackBar sizeTrackBar;
+    }
+}
+
---
+++ Kreis_Animation.1.0/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kreis_Animation._1._0
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        Timer timer;
+
+        float Verschiebung;
+
+        #region Grafik Elemente
+        Pen KreisStift;
+        Brush rectangleBrush;
+
+        Rectangle Kreis;
+        Rectangle Dreieck;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+
+            #region Timer
+            DoubleBuffered = true;
+
+            timer = new Timer
+            {
+                Enabled = true,
+                Interval = 10
+            };
+            timer.Tick += Time_Ticker;
+
+            this.timer.Start();
+            #endregion
+
+            Verschiebung = 0.0f;
+
+            #region Grafik Elemente
+            speedLabel.Text = (speedTrackBar.Value).ToString();
+            radiusLabel.Text = (radiusTrackBar.Value).ToString();
+            sizeLabel.Text = (sizeTrackBar.Value).ToString();
+
+            KreisStift = new Pen(Color.DarkGray, 3);
+            rectangleBrush = new SolidBrush(Color.Black);
+
+            Kreis = new Rectangle(new Point(this.ClientSize.Width / 2 - 100, this.ClientSize.Height / 2 - 100), new Size(200, 200));
+            Dreieck = new Rectangle(new Point(this.ClientSize.Width / 2 - 10, this.ClientSize.Height / 2 - 110), new Size(20, 20));
+            #endregion
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            graphics = e.Graphics;
+
+            graphics.DrawEllipse(KreisStift, Kreis);
+            graphics.FillEllipse(rectangleBrush, Dreieck);
+        }
+
+        private void Time_Ticker(object sender, EventArgs e)
+        {
+            Verschiebung += (float)speedTrackBar.Value / 100;
+
+            Kreis = new Rectangle(new Point(this.ClientSize.Width / 2 - (radiusTrackBar.Value * 50), this.ClientSize.Height / 2 - (radiusTrackBar.Value * 50)), new Size(radiusTrackBar.Value * 100, radiusTrackBar.Value * 100));
+            Dreieck = new Rectangle(new Point(Convert.ToInt32((this.ClientSize.Width / 2 - (radiusTrackBar.Value * 100) - (sizeTrackBar.Value * 10)) + (Math.Sin(Verschiebung) * (radiusTrackBar.Value * 50) + (radiusTrackBar.Value * 100) + sizeTrackBar.Value * 5)), Convert.ToInt32(this.ClientSize.Height / 2 - sizeTrackBar.Value * 5 - Math.Cos(Verschiebung) * (radiusTrackBar.Value * 50))), new Size(sizeTrackBar.Value * 10, sizeTrackBar.Value * 10));
+
+            Invalidate();
+        }
+
+        private void Geschwindigkeits_Trackbar(object sender, EventArgs e)
+        {
+            speedLabel.Text = (speedTrackBar.Value).ToString();
+        }
+
+        private void RadiusTrackbar(object sender, EventArgs e)
+        {
+            radiusLabel.Text = (radiusTrackBar.Value).ToString();
+        }
+
+        private void Größen_Trackbar(object sender, EventArgs e)
+        {
+            sizeLabel.Text = (sizeTrackBar.Value).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
